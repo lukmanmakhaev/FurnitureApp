8d233f035a57da114948c87f81ee3ce85c4df05f
Lukman <lukmanmakhaev@gmail.com>
2023-02-22 14:49:31 +0300

    Initial Commit

---
+++ Furniture/FurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureApp.swift
+//  Furniture
+//
+//  Created by Lukman Makhaev on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
